6177bae442a5c4dc920ceab37bdbcc4e426b3d3d
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-19 18:00:02 -0700

    Add simple Meeting test

---
+++ WWUClassfinder2/src/test/java/edu/wwu/classfinder2/test/data/MeetingTest.java
@@ -0,0 +1,23 @@
+package edu.wwu.classfinder2.test.data;
+
+import edu.wwu.classfinder2.data.Schedule.Meeting;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.joda.time.DateTimeConstants;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.joda.time.LocalTime;
+
+public class MeetingTest extends TestCase {
+
+    public void testMeeting() {
+        Meeting m = new Meeting(DateTimeConstants.MONDAY,
+                                new LocalTime(13,0),
+                                new Period(50, PeriodType.minutes()));
+
+        Assert.assertEquals("M 01:00-01:50 pm", m.toString());
+    }
+
+}
